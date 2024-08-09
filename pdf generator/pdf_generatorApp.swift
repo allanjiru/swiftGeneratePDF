e289e489cd479573e42cb940a99ffbbbb903e48b
allanjiru <allanjiru@gmail.com>
2024-08-09 17:09:08 +0300

    Initial Commit

---
+++ pdf generator/pdf_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  pdf_generatorApp.swift
+//  pdf generator
+//
+//  Created by Allan Njiru on 09/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct pdf_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
